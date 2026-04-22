2c3befe64a4c03e066ee189630bc06ff87997ef9
angelica-hc <ahuamanc@ulasalle.edu.pe>
2026-04-21 21:10:08 -0500

    agregue BubbleSortBase

---
+++ BubbleSortBase.java
@@ -0,0 +1,17 @@
+public class BubbleSortBase {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        int temp;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // intercambio
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
